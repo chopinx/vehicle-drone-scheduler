c107c44490d467899207fb14982cd9cee98b135a
tfl <1844739653@qq.com>
2018-03-19 14:39:20 +0800

    画图的基本类

--- src/com/nest/Main.java
+++ src/com/nest/Main.java
@@ -1,9 +1,20 @@
 package com.nest;
 
+import com.nest.display.Display;
+import com.nest.geometry.Point;
+
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("hello world!");
+        // 示例
+        Point point1 = new Point(2,3);
+        Point point2 = new Point(56,93);
+        Display display = new Display("mdc");
+        display.drawPoint(point1);
+        display.drawPoint(point2, 8, Color.BLUE);
+        display.drawLine(point1,point2);
+        display.drawCircle(300, 300, 50);
     }
 }
