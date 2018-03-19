e4d12496c196dc590cb92a436f947a07dbfc005d
Chopin <xiaoqinbang1993@qq.com>
2018-03-19 15:54:24 +0800

    1.完成tsp接口设计

--- src/com/nest/geometry/Travelable.java
+++ src/com/nest/geometry/Travelable.java
@@ -1,15 +1,16 @@
 package com.nest.geometry;
 
 /**
- * 可用TSP的借口
+ * 可用TSP的接口
  * @author xiaoq
  * @version v1.0
  * @date 2018/3/19
  */
 public interface Travelable {
     /**
-     *
-     * @return 返回到目标的距离
+     * 获取该对象到目标对象的距离
+     * @param des 目标点
+     * @return 返回到目标点的距离
      */
     double getDistance(Travelable des);
 }
